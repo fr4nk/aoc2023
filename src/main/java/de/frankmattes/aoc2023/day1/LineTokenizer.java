720b9926d43165ee5a84384a8421432d64b8f8b7
Frank Mattes <432089+fr4nk@users.noreply.github.com>
2023-12-18 08:13:20 +0100

    Test erweitert
    - Zahlen aus Zeile lesen
    - erste und letzte
    - Zeile decodieren getestet

--- src/main/java/de/frankmattes/aoc2023/day1/LineTokenizer.java
+++ src/main/java/de/frankmattes/aoc2023/day1/LineTokenizer.java
@@ -14,4 +14,17 @@ public class LineTokenizer {
         }).map((int n) -> { return n-48;} ).toArray() ;
         return allDigits;
     }
+
+    public int[] getFirstAndLastDigit() {
+
+        int[] digits = getAllDigits();
+        int[] firstAndLastDigit = new int[] {digits[0], digits[digits.length-1] };
+        return firstAndLastDigit;
+    }
+
+    public int sum() {
+        int[] digits = getFirstAndLastDigit();
+
+        return 10 * digits[0] + digits[1];
+    } 
 }
--- src/test/java/de/frankmattes/aoc2023/day1/TestLineTokenizer.java
+++ src/test/java/de/frankmattes/aoc2023/day1/TestLineTokenizer.java
@@ -4,10 +4,13 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import org.assertj.core.util.Arrays;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 public class TestLineTokenizer {
+
 
     @ParameterizedTest
     @CsvSource(delimiter = ';',value = {
@@ -18,9 +21,37 @@ public class TestLineTokenizer {
     })
     void testLine_allDigitsFound(String input, String digits) {
 
-        LineTokenizer tokenizer = new LineTokenizer(input);
+        LineTokenizer tokenizer = new LineTokenizer(input); 
         int[] foundDigits = tokenizer.getAllDigits();
         assertNotNull(foundDigits);
         assertEquals(digits, Arrays.asList(foundDigits).toString());
+    }
+
+    @ParameterizedTest
+    @CsvSource(delimiter = ';',value = {
+        "1abc2; [1, 2]",
+        "pqr3stu8vwx; [3, 8]"
+        ,"a1b2c3d4e5f; [1, 5]"
+        ,"treb7uchet; [7, 7]"
+    })
+    void TestLineTokenizer_FirstAndLastDigit(String input, String firstAndLastDigit) {
+        LineTokenizer tokenizer = new LineTokenizer(input); 
+     
+        int[] digits = tokenizer.getFirstAndLastDigit();
+        assertNotNull(digits);
+        assertEquals(firstAndLastDigit, Arrays.asList(digits).toString());
+
+    } 
+
+    @ParameterizedTest
+    @CsvSource(delimiter = ';',value = {
+        "1abc2; 12",
+        "pqr3stu8vwx; 38"
+        ,"a1b2c3d4e5f; 15"
+        ,"treb7uchet; 77"
+    })
+    void testSumOfLine(String input, int lineSum) {
+        LineTokenizer tokenizer = new LineTokenizer(input);
+        assertEquals(lineSum, tokenizer.sum());
     }
 }
